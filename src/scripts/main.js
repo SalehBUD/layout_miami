c6c317c7dee61a3c39de5c48be87d16a3b459e9e
Saleh BUD <saleh.7734@gmail.com>
2022-11-17 14:21:22 +0200

    Fix: Scroll behind the menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// const menu = document.querySelector('#menu');
+// const body = document.querySelector('.page__body');
+
+// menu.addEventListener('click', () => {
+
+// });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--no-scroll');
+  } else {
+    document.body.classList.remove('page__body--no-scroll');
+  }
+});
